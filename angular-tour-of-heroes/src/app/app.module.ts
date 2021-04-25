7ee6ca5b23bb86d5f44397e76246618c2d5fdf87
Pavel Foltyn <pavel.foltyn@volny.cz>
2021-04-25 05:54:56 +0200

    TECH Switch two imports in app.module.ts so that their order matches that in the Angular Tutorial.

--- angular-tour-of-heroes/src/app/app.module.ts
+++ angular-tour-of-heroes/src/app/app.module.ts
@@ -1,5 +1,5 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';   // :-- NgModel lives here
 
 import { AppComponent } from './app.component';
