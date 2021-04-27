438048d1b475c61a23dbfea1ca01898fc527df52
Pavel Foltyn <pavel.foltyn@volny.cz>
2021-04-27 05:48:58 +0200

    CHANGE Make the method GetHeroes (of the service HeroService) asynchronous. Use Observable.

--- angular-tour-of-heroes/src/app/hero.service.ts
+++ angular-tour-of-heroes/src/app/hero.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
 
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
@@ -16,10 +17,15 @@ export class HeroService {
 
 
 
-  getHeroes(): Hero[] {
-    return HEROES;
+  //getHeroes(): Hero[] {
+  //  return HEROES;
+  //}
+
+  getHeroes(): Observable<Hero[]> {
+    const heroes = of(HEROES);
+    return heroes;
   }
 
 
-  
+
 }
