1e05e3e03933c39655b2abf7349578fc846b8274
Pavel Foltyn <pavel.foltyn@volny.cz>
2021-04-30 18:58:38 +0200

    TECH Configure the first part of routes.

--- angular-tour-of-heroes/src/app/app-routing.module.ts
+++ angular-tour-of-heroes/src/app/app-routing.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+//import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { HeroesComponent } from './heroes/heroes.component';
+
+
+
+const routes: Routes = [
+  { path: 'heroes', component: HeroesComponent }
+];
 
 
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  //declarations: [],
+  //imports: [
+  //  CommonModule
+  //]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
